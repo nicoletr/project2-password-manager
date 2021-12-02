ecce9aab83a9751238ae43bf3fd0bb9c2dddceea
MA-FS <87353040+MA-FS@users.noreply.github.com>
2021-12-02 20:10:00 +0800

    First upload with handlebars layouts and partials

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-const path = require('path');
-const express = require('express');
-const exphbs = require('express-handlebars');
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
-const sequelize = require('./config/connection');
+const sequelize = require("./config/connection");
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.set("view engine", "handlebars");
+
+app.engine("handlebars", hbs.engine);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening at http://localhost:3001'));
+  app.listen(PORT, () => console.log("Now listening at http://localhost:3001"));
 });
+
+// -----Temp routing for handlebars testing-----
+
+app.get("/", async (req, res) => {
+  res.render("applist", { layout: "index" });
+});
+
+app.get("/test", async (req, res) => {
+  res.render("test", { layout: "index" });
+});
+
+// ---------------------------------------------
