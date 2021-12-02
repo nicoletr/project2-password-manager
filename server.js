3f43fa165f4a8e749ed9b69aed8d610b350c2351
MA-FS <87353040+MA-FS@users.noreply.github.com>
2021-12-02 21:27:32 +0800

    New login handlebars page

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const path = require("path");
-const express = require("express");
-const exphbs = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
-const sequelize = require("./config/connection");
+const sequelize = require('./config/connection');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
@@ -24,27 +24,31 @@ const sess = {
 
 app.use(session(sess));
 
-app.set("view engine", "handlebars");
-app.engine("handlebars", hbs.engine);
+app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Come save your passwords at http://localhost:${PORT}!\nIf/When you get hacked, we take no responsibility :)`));
+  app.listen(PORT, () =>
+    console.log(
+      `Come save your passwords at http://localhost:${PORT}!\nIf/When you get hacked, we take no responsibility :)`
+    )
+  );
 });
 
 // -----Temp routing for handlebars testing-----
 
-app.get("/", async (req, res) => {
-  res.render("applist", { layout: "index" });
-});
+// app.get("/", async (req, res) => {
+//   res.render("applist", { layout: "index" });
+// });
 
-app.get("/test", async (req, res) => {
-  res.render("test", { layout: "index" });
-});
+// app.get("/test", async (req, res) => {
+//   res.render("test", { layout: "index" });
+// });
 
 // ---------------------------------------------
